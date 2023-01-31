eab165c8eb1a52732a8016fe505cec6631898759
Pooja Govindharajan <phareesh194@gmail.com>
2023-01-31 12:11:29 -0500

    Fixing swagger error

--- AmigosAPI/Controllers/UserController.cs
+++ AmigosAPI/Controllers/UserController.cs
@@ -98,6 +98,7 @@ namespace AmigosAPI.Controllers
         }
 
         [Route("AddUserMany")]
+        [HttpPost]
         public ActionResult<List<UserDTO>> AddUser(List<NewUserDTO> users)
         {
             try
